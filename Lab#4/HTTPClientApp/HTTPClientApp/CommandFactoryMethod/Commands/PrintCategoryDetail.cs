4ae6de557b9ee95aa9ae9671be75f89eca7180e7
Bargan Constantin <chinchillemd@gmail.com>
2023-04-12 11:59:31 +0300

    impliment get category details

--- Lab#4/HTTPClientApp/HTTPClientApp/CommandFactoryMethod/Commands/PrintCategoryDetail.cs
+++ Lab#4/HTTPClientApp/HTTPClientApp/CommandFactoryMethod/Commands/PrintCategoryDetail.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,34 @@ namespace HTTPClientApp.CommandFactoryMethod.Commands
     {
         public void Execute()
         {
-            throw new NotImplementedException();
+            var response = HttpClientService.GetCategories();
+            Console.Clear();
+
+            if (response.Status)
+            {
+                ConsoleUtils.PrintWithColour("View Category Details\nCategory name: ", ConsoleColor.DarkBlue);
+                var categoryName = Console.ReadLine();
+
+                bool categoryFound = false;
+ 
+                foreach (var category in response.Categories)
+                {
+                    if (category.name.ToLower().Contains(categoryName.ToLower()))
+                    {
+                        ConsoleUtils.PrintWithColour($" Id: {category.id} \n Name: {category.name} \n ItemsCount: {category.itemsCount}\n", ConsoleColor.DarkGreen);
+                        categoryFound = true;
+                    }
+                }
+
+                if (!categoryFound) ConsoleUtils.PrintWithColour("Category not found!", ConsoleColor.DarkRed);
+            }
+            else
+            {
+                ConsoleUtils.PrintWithColour(response.StatusMsg, ConsoleColor.DarkRed);
+            }
+
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
