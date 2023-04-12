7f053ec682429883fd35eaf8d2192c3dc51ff2ee
Bargan Constantin <chinchillemd@gmail.com>
2023-04-12 11:15:22 +0300

    add new folder for constants

---
+++ Lab#4/HTTPClientApp/HTTPClientApp/Constants/UrlAddresses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClientApp.Constants
+{
+    public static class UrlAddresses
+    {
+        public static string BaseAddress = "http://localhost:5000";
+        public static string GetCategoryListAddress = "/api/Category/categories";
+    }
+}
