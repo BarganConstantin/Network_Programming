08c90aaf3e683064622ccc83d5f93640d35f927f
Bargan Constantin <chinchillemd@gmail.com>
2023-04-12 11:23:11 +0300

    separe entities and view models

---
+++ Lab#4/HTTPClientApp/HTTPClientApp/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClientApp.Entities
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int itemsCount { get; set; }
+    }
+}
---
+++ Lab#4/HTTPClientApp/HTTPClientApp/ViewModels/GetCategoriesResponse.cs
@@ -0,0 +1,16 @@
+﻿using HTTPClientApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClientApp.ViewModels
+{
+    public class CategoryListResponse
+    {
+        public bool Status { get; set; }
+        public string StatusMsg { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
