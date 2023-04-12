d7df4e1900452f0cad98de3b25fc4cb80de681d2
Bargan Constantin <chinchillemd@gmail.com>
2023-04-12 11:21:02 +0300

    impliment get categories from api functional

--- Lab#4/HTTPClientApp/HTTPClientApp/CommandFactoryMethod/Commands/PrintCategoryList.cs
+++ Lab#4/HTTPClientApp/HTTPClientApp/CommandFactoryMethod/Commands/PrintCategoryList.cs
@@ -10,7 +10,31 @@ namespace HTTPClientApp.CommandFactoryMethod.Commands
     {
         public void Execute()
         {
-            throw new NotImplementedException();
+            var response = HttpClientService.GetCategories();
+            Console.Clear();
+
+            if (response.Status) 
+            {
+                if (response.Categories.Count > 0) 
+                {
+                    Console.WriteLine("Available Category:");
+                    foreach (var category in response.Categories)
+                    {
+                        Console.WriteLine($" - {category.name}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Service don't have any category!");
+                }
+            }
+            else
+            {
+                Console.WriteLine(response.StatusMsg);
+            }
+
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
--- Lab#4/HTTPClientApp/HTTPClientApp/HttpClientService.cs
+++ Lab#4/HTTPClientApp/HTTPClientApp/HttpClientService.cs
@@ -1,42 +1,46 @@
-﻿using System;
+﻿using HTTPClientApp.Constants;
+using HTTPClientApp.Entities;
+using HTTPClientApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HTTPClientApp
 {
     public class HttpClientService
     {
-        private readonly HttpClient _httpClient;
+        private static readonly HttpClient _httpClient = new HttpClient();
 
-        public HttpClientService(HttpClient httpClient, Uri baseUrlAddress) 
+        static HttpClientService() 
         {
-            _httpClient = httpClient;
-            _httpClient.BaseAddress = baseUrlAddress;
-
+            _httpClient.BaseAddress = new Uri(UrlAddresses.BaseAddress);
             setAcceptJsonHeaderFormat();
         }
 
-        public void Run()
+        public static CategoryListResponse GetCategories()
         {
-
-            HttpResponseMessage response = makeRequest("/api/Category/categories");
+            HttpResponseMessage response = getRequest(UrlAddresses.GetCategoryListAddress);
+            Task<string> responseBody;
 
             if (response.IsSuccessStatusCode)
             {
-                Task<string> responseBody = getResponseBodyAsync(response);
-                Console.WriteLine(responseBody.Result);
+                responseBody = getResponseBodyAsync(response);
+                var categories = JsonSerializer.Deserialize<List<Category>>(responseBody.Result);
+                return new CategoryListResponse() { Status = true, Categories = categories};
             }
             else
             {
-                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                var msg = ("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase).ToString();
+                return new CategoryListResponse() { Status = false , StatusMsg = msg};
             }
         }
 
-        private HttpResponseMessage makeRequest(string url) 
+        private static HttpResponseMessage getRequest(string url) 
         {
             var response = _httpClient.GetAsync("/api/Category/categories").Result;
             return response;
@@ -48,14 +52,14 @@ namespace HTTPClientApp
             return response;
         }
 
-        private async Task<string> getResponseBodyAsync(HttpResponseMessage response)
+        private static async Task<string> getResponseBodyAsync(HttpResponseMessage response)
         {
             string responseBody = await response.Content.ReadAsStringAsync();
 
             return responseBody;
         }
 
-        private void setAcceptJsonHeaderFormat()
+        private static void setAcceptJsonHeaderFormat()
         {
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
